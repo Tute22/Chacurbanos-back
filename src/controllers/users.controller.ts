db2811e39a09d3789e67ca55d74fe8e5fafe4853
Gastón <garabinovich@gmail.com>
2023-11-29 19:14:45 -0300

    user can now login and register

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { UsersService } from '../services/users.service';
+import { Controller, Get, Post, Body } from '@nestjs/common'
+import { UsersService } from '../services/users.service'
+import { CreateUserDto } from 'src/types/users.dto'
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  allUsers() {
-    return this.usersService.getAllUsers();
-  }
+    @Get()
+    allUsers() {
+        return this.usersService.getAllUsers()
+    }
+
+    @Post()
+    createNewUser(@Body() newUser: CreateUserDto) {
+        console.log(newUser)
+        return this.usersService.createUser(
+            newUser.name,
+            newUser.lastName,
+            newUser.email,
+            newUser.password
+        )
+    }
+
+    @Post('login')
+    loginUser(@Body() credentials: { email: string; password: string }) {
+        return this.usersService.login(credentials.email, credentials.password)
+    }
 }
--- src/routes/users.module.ts
+++ src/routes/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from '../controllers/users.controller';
-import { UsersService } from '../services/users.service';
+import { Module } from '@nestjs/common'
+import { UsersController } from '../controllers/users.controller'
+import { UsersService } from '../services/users.service'
+import { JWTtoken } from 'src/config/token'
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+    controllers: [UsersController],
+    providers: [UsersService, JWTtoken],
 })
 export class UsersModule {}
