ba057f636117623fe103aa6868abe61dd0aa970e
Gastón <garabinovich@gmail.com>
2023-11-30 18:15:56 -0300

    token decoding

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Controller, Get, Post, Body, Param } from '@nestjs/common'
 import { UsersService } from '../services/users.service'
+import { JWTtoken } from 'src/config/token'
 import { CreateUserDto } from 'src/types/users.dto'
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtToken: JWTtoken
+    ) {}
 
     @Get()
     allUsers() {
@@ -24,5 +28,16 @@ export class UsersController {
     @Post('login')
     loginUser(@Body() credentials: { email: string; password: string }) {
         return this.usersService.login(credentials.email, credentials.password)
+    }
+
+    @Get(':token')
+    validateToken(@Param('token') token: string) {
+        const decodedToken = this.jwtToken.validateToken(token)
+
+        if (decodedToken) {
+            return { decodedToken }
+        } else {
+            return { message: 'Token inválido' }
+        }
     }
 }
