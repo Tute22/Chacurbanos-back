81d72559b21e4ab097e54a3634ebf62762204125
Agustin Sandoval <agustinsandoval1601@gmail.com>
2024-01-09 19:39:15 -0300

    add swagger

--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -11,28 +11,67 @@ import {
 } from '@nestjs/common'
 import { CreatePackagesDto } from './packages.dto'
 import { PackagesService } from './packages.service'
+import { ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger'
 
 @Controller('packages')
+@ApiTags('packages')
 export class PackagesController {
     constructor(private readonly packagesService: PackagesService) {}
 
     @Get()
+    @ApiResponse({ status: 200, description: 'Returns all packages.' })
     allPackages() {
         return this.packagesService.getAllPackages()
     }
 
     @Get(':_id')
+    @ApiResponse({
+        status: 200,
+        description: 'Returns a specific package by id.',
+    })
     async getPackageById(@Param('_id') packageId: string) {
         return this.packagesService.getPackageById(packageId)
     }
 
     @Post()
+    @ApiResponse({ status: 201, description: 'Creates a package.' })
+    @ApiBody({
+        description: 'Package creation example.',
+        type: CreatePackagesDto,
+        examples: {
+            default: {
+                summary: 'Body for package creation example.',
+                value: {
+                    address: 'Chaco 123',
+                    recipient: 'Pipa Sandoval',
+                    weight: 2.5,
+                    date: '2024-01-15',
+                },
+            },
+        },
+    })
     @HttpCode(HttpStatus.CREATED)
     createNewPackage(@Body() newPackage: CreatePackagesDto) {
         return this.packagesService.createPackage(newPackage)
     }
 
     @Patch(':_id')
+    @ApiResponse({
+        status: 201,
+        description: 'Updates a specific package by id and returns it.',
+    })
+    @ApiBody({
+        description: 'Package update example.',
+        type: CreatePackagesDto,
+        examples: {
+            default: {
+                summary: 'Body to update a package example.',
+                value: {
+                    weight: 5,
+                },
+            },
+        },
+    })
     @HttpCode(HttpStatus.CREATED)
     updatePackage(
         @Param('_id') packageId: string,
@@ -51,6 +90,10 @@ export class PackagesController {
     }
 
     @Delete(':_id')
+    @ApiResponse({
+        status: 200,
+        description: 'Deletes a specific package by id.',
+    })
     deletePackage(@Param('_id') packageId: string) {
         const result = this.packagesService.deletePackage(packageId)
 
--- src/packages/packages.dto.ts
+++ src/packages/packages.dto.ts
@@ -1,6 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class CreatePackagesDto {
+    @ApiProperty({
+        description: "The recipient's address",
+        example: '123 Main Street, City, Country',
+    })
     address: string
+
+    @ApiProperty({
+        description: 'Name of the recipient',
+        example: 'Luis Advincula',
+    })
     recipient: string
-    weight: string
+
+    @ApiProperty({
+        description: 'Weight of the package',
+        example: 2,
+        type: Number,
+    })
+    weight: number
+
+    @ApiProperty({
+        description: 'Date of the package shipment',
+        example: '2024-01-15',
+        type: Date,
+    })
     date: Date
 }
